006340ce60587219d588831ae53e57d5152089cb
Tien Pham <tien@cloudkinetix.com>
2024-04-10 10:07:35 +0700

    Init project

---
+++ WatchTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchTV
+//
+//  Created by Tien Pham on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
